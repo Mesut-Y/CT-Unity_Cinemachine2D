e40ceb6723f4e10bd0ce8ae4f1c3e7da1230e24b
mesutyanmaz <71411632+mesutyanmaz@users.noreply.github.com>
2025-02-05 18:28:02 +0300

    Player hareket kodlaması yapıldı.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xInput = Input.GetAxis("Horizontal"); //inputmanager ekranında tuş atamaları yapılmış.
+        float yInput = Input.GetAxis("Vertical");
+
+        Vector2 direction = new Vector2(xInput, yInput).normalized; //çapraz yönde hızı artmasın diye normalized yapıldı.
+
+        player.linearVelocity = direction * 2f; //karakter hızı 2f ile arttırıldı.
+    }
+}
